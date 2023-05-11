63af5de07a7f0ac528b5e75f5d55414587708c11
gas-limit <100609687+gas-limit@users.noreply.github.com>
2023-05-11 02:42:04 -0400

    Update UserFactory.sol

--- UserFactory.sol
+++ UserFactory.sol
@@ -2,10 +2,6 @@
 pragma solidity ^0.8.9;
 import "./UserPass.sol";
 
-/**
- * @title UserFactory
- * @dev A contract for creating and managing user accounts and associated creator passes.
- */
 contract UserFactory {
 
     address immutable admin;
@@ -15,7 +11,7 @@ contract UserFactory {
     mapping(address => uint40) public AddressToID;
     mapping(address => creatorData) public AddressToCreatorData;
 
-    uint8 IDCounter = 1;
+    uint40 IDCounter = 1;
 
     struct creatorData {
         address creatorAddress;
@@ -25,6 +21,7 @@ contract UserFactory {
         address bronzeAddress;
         address silveraddress;
         address goldAddress;
+
     }
 
     struct videoData {
@@ -37,37 +34,21 @@ contract UserFactory {
 
     mapping(address => videoData[]) userVideoData;
 
-    /**
-     * @dev Initializes the UserFactory contract and sets the admin address.
-     */
-    constructor() {
+
+
+    constructor(){
         admin = msg.sender;
     }
 
-    /**
-     * @dev Creates a new creator account and associated creator passes.
-     * @param _creatorName The name of the creator.
-     * @param _description The description of the creator.
-     * @param _bronzePrice The price of the bronze subscription pass.
-     * @param _silverPrice The price of the silver subscription pass.
-     * @param _goldPrice The price of the gold subscription pass.
-     */
-    function makeCreator(
-        string memory _creatorName,
-        string memory _description,
-        uint _bronzePrice,
-        uint _silverPrice,
-        uint _goldPrice
-    ) public {
+    function makeCreator(string memory _creatorName, string memory _description, uint _bronzePrice, uint _silverPrice, uint _goldPrice ) public  {
 
-        uint8 IDCounter_ = IDCounter;
+        uint40 IDCounter_ = IDCounter;
 
-        // Deploy new CreatorPass contracts
+        
         CreatorPass bronzeNFT = new CreatorPass(_creatorName, msg.sender, _bronzePrice);
         CreatorPass silverNFT = new CreatorPass(_creatorName, msg.sender, _silverPrice);
         CreatorPass goldNFT = new CreatorPass(_creatorName, msg.sender, _goldPrice);
 
-        // Store creator data
         creatorData memory creatorData_ = creatorData(
             msg.sender,
             IDCounter,
@@ -80,28 +61,16 @@ contract UserFactory {
 
         AddressToCreatorData[msg.sender] = creatorData_;
 
-        // Map ID to address and address to ID
         IDToAddress[IDCounter_] = msg.sender;
         AddressToID[msg.sender] = IDCounter_;
 
         creators.push(msg.sender);
 
         ++numCreators;
+
     }
 
-    /**
-     * @dev Adds a video to the user's account.
-     * @param _name The name of the video.
-     * @param _videoURL The URL of the video.
-     * @param _description The description of the video.
-     * @param _tier The tier of the video (0 for bronze, 1 for silver, 2 for gold).
-     */
-    function addVideo(
-        string memory _name,
-        string memory _videoURL,
-        string memory _description,
-        uint8 _tier
-    ) public {
+    function addVideo(string memory _name, string memory _videoURL, string memory _description, uint8 _tier) public {
         videoData memory videoData_ = videoData(_name, _videoURL, _description, _tier);
 
         userVideoData[msg.sender].push(videoData_);
